958ffced7a3236c399dda6541141b7a140d1c8e4
matiasea <mati.almada02@gmail.com>
2023-03-05 21:44:45 -0300

    "commit 5/3"

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -4,7 +4,7 @@ import { Router } from "express";
 import { Cart } from "../data/dbCart.js";
 import { FileManager } from "../data/db.js";
 
-const productManager = new FileManager("./data/cart.json");
+const productManager = new FileManager("./data/products.json");
 const carts = new Cart("./data/cart.json");
 const route = Router();
 
@@ -32,28 +32,38 @@ route.post("/" , async (req, res) => {
     }res.send({searchId})
  })
 
- route.post("/:cid/products/:pid" , async (req, res) => {
-    const cid = req.params.cid;
-    const pid = req.params.pid;
-    const allCarts = await carts.getAllCarts();
-    const searchIdCart = allCarts.find( cart => cart.id == cid);
-    console.log(searchIdCart)
-    if(!searchIdCart){
-        res.status(404).send({error: `Carrito con Id ${cid} no encontrado`});
-       
-    }
-    const allProducts = await productManager.getAll();
-    console.log(allProducts);
-    const searchIdProduct = allProducts.find(product => product.id == pid);
-    console.log({searchIdProduct} )
-    if(!searchIdProduct){
-        res.status(404).send({error: `Producto con Id ${pid} no encontrado`});
-        return;
-    }
-    searchIdCart.productCart.push(searchIdProduct)
-   
 
- })
+    route.post('/:cid/product/:pid', async (req, res) => {
+        const {cid, pid} = req.params;
+        const allCarts = await carts.getAllCarts();
+        const cartFound = allCarts.find(cart => cart.id == cid);
+        if (!cartFound){
+            res.status(404).send({ error: `no existe el carrito con el id  ${cid}`});
+            return
+        }
+    
+        const productFound = await productManager.getById(pid);
+        if (!productFound){
+            res.status(404).send({ error: `no existe el producto con el id  ${pid}`});
+            return
+        }
+    
+        const productInCart = cartFound.productCart.filter(productCart =>productCart.productCart == pid)
+        const productOutCart = cartFound.productCart.filter(productCart =>productCart.productCart != pid)
+        let quantityUpdate = 1;
+        if (productInCart.length > 0){
+            const {quantity} = productInCart[0];
+            quantityUpdate = quantity +1;
+        }
+    
+        const newProduct = {"product": pid, "quantity": quantityUpdate}
+        const products = { productCart: [...productOutCart, newProduct]}
+        await productManager.update(cid, products);
+        
+        res.status(200).send({ok: true});
+    });
+
+ 
 
 
  export default route;
